18149a91a2eb979c0e6d5f1b687cdaf82ebb7015
Carlos Bolzanell <carlosbolzanell@gmail.com>
2023-10-31 16:38:55 -0300

    .

--- PixelBattle/core/src/screens/GameScreen.java
+++ PixelBattle/core/src/screens/GameScreen.java
@@ -27,8 +27,8 @@ public class GameScreen extends ScreenAdapter {
     public void show() {
     	batch = new SpriteBatch();
 		cenario = new Texture("Cenario.png");
-		personagem1 = new PersonagemEsquerda("./xitxor/esquerda/padrao.png", 10, 87, 500, new Ataque("./xitxor/esquerda/ataque.png", 20, 20), new SuperAtaque("./xitxor/esquerda/especial.png", 10, 10));
-		personagem2 = new PersonagemDireita("./bell/direita/padrao.png", 1000, 87, 500, new Ataque("./bell/direita/ataque.png", 20, 20), new SuperAtaque("./bell/direita/especial.png", 10, 10));
+		personagem1 = new PersonagemEsquerda("./xitxor/esquerda/padrao.png", 10, 87, 500, new Ataque("./xitxor/esquerda/ataque.png", 20, 20), new SuperAtaque("./xitxor/esquerda/especial.png", 10, 40));
+		personagem2 = new PersonagemDireita("./bell/direita/padrao.png", 1000, 87, 500, new Ataque("./bell/direita/ataque.png", 20, 20), new SuperAtaque("./bell/direita/especial.png", 10, 40));
 		retangulo = new Texture("branco.png");
     }
 
@@ -105,15 +105,16 @@ public class GameScreen extends ScreenAdapter {
 			}		
 		}
 		
-//		if(personagem1.isSuperAtacando()) {
-//			Rectangle ataque1Bounds = new Rectangle(personagem1.getSuperAtaque().getSprite().getX(), personagem1.getSuperAtaque().getSprite().getY(), personagem1.getSuperAtaque().getSprite().getWidth(), personagem1.getSuperAtaque().getSprite().getHeight());
-//			
-//			if(ataque1Bounds.overlaps(personagem1Bounds)) {
-//				tamanho2 -= personagem1.getSuperAtaque().getDano();
-//				personagem2.setVida(personagem2.getVida() - personagem1.getSuperAtaque().getDano());
-//				personagem1.atacou();
-//			}		
-//		}
+		if(personagem1.isSuperAtacando()) {
+			Rectangle ataque1Bounds = new Rectangle(personagem1.getSuperAtaque().getSprite().getX(), personagem1.getSuperAtaque().getSprite().getY(), personagem1.getSuperAtaque().getSprite().getWidth(), personagem1.getSuperAtaque().getSprite().getHeight());
+			
+			if(ataque1Bounds.overlaps(personagem2Bounds)) {
+				tamanho2 -= personagem1.getSuperAtaque().getDano();
+				position += personagem1.getSuperAtaque().getDano();
+				personagem2.setVida(personagem2.getVida() - personagem1.getSuperAtaque().getDano());
+				personagem1.atacou();
+			}		
+		}
 		
 		personagem1.setColidiu( (personagem1Bounds.overlaps(personagem2Bounds)) ? true : false);
 		personagem2.setColidiu( (personagem2Bounds.overlaps(personagem1Bounds)) ? true : false);
